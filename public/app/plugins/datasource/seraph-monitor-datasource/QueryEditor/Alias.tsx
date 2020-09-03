312fbe254ba5577dcfc467cccb230ca072b67718
stbui <w431106@163.com>
2020-09-03 16:35:13 +0800

    test: setting

--- public/app/plugins/datasource/seraph-monitor-datasource/QueryEditor/Alias.tsx
+++ public/app/plugins/datasource/seraph-monitor-datasource/QueryEditor/Alias.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default ({ defaultValue, value, onChange }: any) => {
   return (
-    <div className="gf-form-inline">
+    <div className="gf-form-inline" style={{ display: 'none' }}>
       <div className="gf-form">
         <label className="gf-form-label query-keyword width-7">别名</label>
       </div>
